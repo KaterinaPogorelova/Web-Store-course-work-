88a0071b5f2a8a7d5a5f0c2d64d4b2d4023132a6
polishukova <pollimagenta@gmail.com>
2023-03-11 16:24:03 +0100

    js first steps

---
+++ src/js/basket.js
@@ -0,0 +1,25 @@
+const basket = document.querySelector('.header__basket--wrapper');
+const basketMenu = document.querySelector('.shopping__bag');
+const basketBtn = document.querySelector('.header__basket');
+
+//открывает корзину по клику
+export const getBasket = basket.addEventListener('click', () =>{
+    basketBtn.classList.toggle('basket__active');
+    basketMenu.classList.toggle('shopping__bag--active');
+})
+
+let cards = [];
+localStorage.setItem('cart', JSON.stringify(cards));
+
+//отрисовка карточек на странице
+export const getCards = () => {
+    cards = JSON.parse(localStorage.getItem('cart'));
+    if (cards.length === 0){
+        const emptyBag = document.querySelector('.shopping__bag-empty');
+        emptyBag.classList.add('shopping__bag-notempty');
+        const trashcan = document.querySelector('.shopping__bag-trashcan');
+        trashcan.classList.add('trashcan__empty');
+    }
+}
+
+getCards()
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,3 @@
 import { swiper } from './swiper.js';
+import { getBasket } from './basket.js';
+import { getCards } from './basket.js';
